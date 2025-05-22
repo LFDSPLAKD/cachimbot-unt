6238f745e8e82a77a5c0fd44a15e9236a1c080db
LFDSPLAKD <mentecolmenaxgpt@gmail.com>
2025-05-22 00:31:41 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+# Instalar todas las dependencias desde requirements.txt
+pip install -r requirements.txt
+
+# Configurar Flask para que se ejecute correctamente
 export FLASK_APP=app.py
+
+# Ejecutar la aplicación Flask
 flask run --host=0.0.0.0 --port=10000
